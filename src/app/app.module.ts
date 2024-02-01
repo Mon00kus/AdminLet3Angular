6d8814307bb12e02356e32efb73cd3ec598a6a31
mon00kus <raulmoncadac@gmail.com>
2024-02-01 12:35:25 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopageFoundComponent,
+    PagesComponent
+    
   ],
   imports: [
     BrowserModule,
